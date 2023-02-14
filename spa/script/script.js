4ccb54d64039f3962911ac5ac1a79f78c42e9427
Eva Zaadnoordijk <evazaadnoordijk@gmail.com>
2023-02-14 21:02:44 +0100

    changed api

--- spa/script/script.js
+++ spa/script/script.js
@@ -1,6 +1,7 @@
 // variables
 var daily = document.querySelector("section:first-of-type q");
-var randomness = getRandomInt(11);
+var refreshButton = document.querySelector("section:first-of-type button");
+// var randomness = getRandomInt(11);
 
 
 
@@ -8,44 +9,58 @@ var randomness = getRandomInt(11);
 // logica
 fetchData()
 
-
+// eventlisteners
+refreshButton.addEventListener("click", function() {
+    location.reload()
+}, false)
 
 
 // functions
 function fetchData (){
-    const url = 'https://quote.api.fdnd.nl/v1/quote'
+    var category = 'amazing';
+    // const url1 = `https://api.api-ninjas.com/v1/quotes?category=${category}&limit=10`;
+    const options = {
+        // method: 'GET',
+        headers: {'X-Api-Key': 'mr1axNSXq9DjWUuEBQUHGA==YK6lI63UGnsVpLyt'}
+    };
 
-    const data = fetch(url)
+    const data = fetch(url1, options)
                     .then(response => response.json())
                     .then(data => {
                         // iets gaan doen met de data
                         // data, h1 veranderen naar naam
-                        changeHTML(data)
-                        console.log(data.data);
+                        // changeHTML(data);
+                        
+                        console.log(data);
+                        daily.textContent = data[0].quote;
                     })
     
 }
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-// console.log(getRandomInt(11));
+// function getRandomInt(max) {
+//     return Math.floor(Math.random() * max);
+//   }
+// // console.log(getRandomInt(11));
 
-function changeHTML (data) {
+// function changeHTML (data) {
   
-        data.data.forEach( element => {
-        if(element.quoteId == randomness){
-            daily.textContent = element.text;
-            // avatar.src = element.avatar;
-            // website.href = element.website;
-            console.log(element.text);
-            }
-        });
+//         data.data.forEach( element => {
+//         if(element.category == randomness){
+//             // daily.textContent = element.text;
+//             // avatar.src = element.avatar;
+//             // website.href = element.website;
+//             console.log(element.text);
+//             }
+//         });
     
-}
+// }
 
 
+// function getRandomQuote(data){
+//     // get ranom item from array
 
+//     changeHTML(quote)
+// }
 
 
 // function getRandomArbitrary(min, max) {
